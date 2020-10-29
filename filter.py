0780d43a5064ddce1b4b2a8a385593b2e868167e
Wouter van Rijmenam <woutervanrijmenam@MacBook-Pro.local>
2020-10-29 16:51:57 +0100

    fixed duplicates and wrong scores

--- filter.py
+++ filter.py
@@ -1,48 +1,30 @@
 import pandas as pd
-
 data = pd.read_csv("/Users/woutervanrijmenam/stack/02_Projects/04_Sites/RSL/data/userReviews.csv", sep=";")
-
 # print(data.head())
 # print(data[:3])
 # print(data.movieName.iloc[1])
-
 column_names = ['movieName', 'Metascore_w', 'Author', 'AuthorHref', 'Summary','origin_rating']
-
-subset = pd.DataFrame(columns = column_names)
 author_db = pd.DataFrame(columns = column_names)
-
 recomendations = pd.DataFrame(columns = column_names)
 
-# for movie in range(100):
-#     if data.movieName.iloc[movie] == "beach-rats":
-#         row=data[movie:movie + 1]
-#         print(row['Author'])
-#         subset.append(row)
+for i, r in data.iterrows():
+    if data.movieName.iloc[i] == "beach-rats":
+        current_rating = r["Metascore_w"]
+        author_db = author_db.append(pd.DataFrame({'movieName': r['movieName'], 'Metascore_w': r['Metascore_w'] , 'Author': r['Author'], 'AuthorHref': r['AuthorHref'], 'Summary': r['Summary'], 'origin_rating': current_rating}, index=[0]), ignore_index=True)
 
-for index, row in data.iterrows():
-    if data.movieName.iloc[index] == "beach-rats":
-        print(row["Author"])
-        # author_name = row["Author"]
+for index, r in author_db.iterrows():
+    author = r["Author"]
+    current_rating = r["origin_rating"]
 
-        # print(row["Summary"])
-        current_rating = row["Metascore_w"]
-        # if(row["Metascore_w"] >= 6):
-        subset = subset.append(pd.DataFrame({'movieName': row['movieName'], 'Metascore_w': row['Metascore_w'] , 'Author': row['Author'], 'AuthorHref': row['AuthorHref'], 'Summary': row['Summary']}, index=[0]), ignore_index=True)
-
-        for i,r in data.iterrows():
-            if data.Author.iloc[i] == row["Author"]:
-                author_db = author_db.append(pd.DataFrame({'movieName': r['movieName'], 'Metascore_w': r['Metascore_w'] , 'Author': r['Author'], 'AuthorHref': r['AuthorHref'], 'Summary': r['Summary']}, index=[0]), ignore_index=True)
-
-                
-        for i,r in author_db.iterrows():
-            if r['Metascore_w'] > current_rating:
+    print(author)
+    print(current_rating)
+    for i, r in data.iterrows():
+        if data.Author.iloc[i] == author:
+            if r["Metascore_w"] > current_rating:
+                print("--", r['movieName'], "-", r["Metascore_w"])
                 recomendations = recomendations.append(pd.DataFrame({'movieName': r['movieName'], 'Metascore_w': r['Metascore_w'] , 'Author': r['Author'], 'AuthorHref': r['AuthorHref'], 'Summary': r['Summary'], 'origin_rating': current_rating}, index=[0]), ignore_index=True)
 
 
-
-
-# print("--- subset coming -----")
-print(author_db)
 # print("--- Recom coming -----")
 # print(recomendations)
 recomendations.to_csv("recomendations.csv", sep=';')
